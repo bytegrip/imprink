6e98b7dc226580fd63285e3451c16248abece1cc
lumijiez <59575049+lumijiez@users.noreply.github.com>
2025-04-28 19:26:58 +0300

    Add Test projects

---
+++ tests/Printbase.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Printbase.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
---
+++ tests/Printbase.WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Printbase.WebApi.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
